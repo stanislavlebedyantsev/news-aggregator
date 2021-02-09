eedf06cc4e6572467cca8e806032bc8b8edc2c3d
stanislavlebedyantsev <stanislav.lebedyantsev@gmail.com>
2021-02-09 15:35:59 +0300

    Added folders for work

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,27 +1,13 @@
 import './App.css';
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
-import {store} from "./redux/reduxStore";
+import {store} from "./reducers/reduxStore";
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <div className="App">
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        
       </Provider>
     </BrowserRouter>
 
